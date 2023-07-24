9416331eda5a27b09ec66f27bf1b9717103984b8
sheritsh <sheritsh@student.21-school.ru>
2023-07-25 02:09:33 +0300

    mock: placeholder for task

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -7,3 +7,6 @@ const apiController = new Api();
 const getCars = await apiController.getWinners();
 
 console.log(getCars.winners);
+
+// eslint-disable-next-line no-alert
+alert('Здравствуй!\nЕсли ты вдруг проснулся с утра по раньше\nи решил проверить мою работу, то пожалуйста,\nподожди до обеда я все зарефакторю и обновлю gh-pages\nОГРОМНОЕ СПАСИБО ЗА ПОНИМАНИЕ\n25.07.2023');
