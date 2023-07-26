5299115a9ff3dd8079eb3a773b10183e4fa773ff
sheritsh <sheritsh@student.21-school.ru>
2023-07-26 09:02:01 +0300

    rc: add footer and delete console.logs

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -2,6 +2,3 @@ import App from './app/app';
 import './app/styles/style.css';
 
 App.composePage();
-console.log('Thank U and GL');
-console.log('created by // sheritsh');
-console.log('Yesli bi vi znali kak ya zaebalsya');
